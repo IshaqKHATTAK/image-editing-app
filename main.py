4a6d30126ad821123a40c43193c223b0819efbba
IshaqKHATTAK <mi5315562@gmail.com>
2023-09-04 05:21:42 -0700

    HTML added

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 app.run(debug=True)
